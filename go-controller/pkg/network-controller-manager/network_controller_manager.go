38e2ebb97b818d678f13d39395390fe8c0423326
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2024-07-16 14:52:17 +0000

    Get route advertisement information in NAD controller
    The network manager running within the NAD controller will, upon
    ensuring a network, fetch the VRFs per node a pod network is being
    leaked/advertised to from the applicable route advertisements
    configuration, and include it in the network information used when
    creating a network controller, or triggering a reconciliation if it was
    already running.
    
    This relies on annotations set by cluster manager on NADs pointing to
    the route advertising configuration that applies to the network which
    will come in a future PR/commit.
    
    This includes the default network for which the ever existing default
    network controller is used (instead of creating a new network
    controller). If necessary, it is assumed that cluster manager will
    create a dummy NAD for the default network in ovn-k namespace to set
    annotations on. If no NADs for the default network exist or if they have
    no annotations, network manager will reconcile the default network to a
    default configuration (instead of destroying the network controller).
    
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- go-controller/pkg/network-controller-manager/network_controller_manager.go
+++ go-controller/pkg/network-controller-manager/network_controller_manager.go
@@ -54,7 +54,7 @@ type NetworkControllerManager struct {
 	defaultNetworkController nad.BaseNetworkController
 
 	// net-attach-def controller handle net-attach-def and create/delete network controllers
-	nadController *nad.NetAttachDefinitionController
+	nadController nad.NADController
 }
 
 func (cm *NetworkControllerManager) NewNetworkController(nInfo util.NetInfo) (nad.NetworkController, error) {
@@ -211,11 +211,12 @@ func NewNetworkControllerManager(ovnClient *util.OVNClientset, wf *factory.Watch
 
 	var err error
 	if config.OVNKubernetesFeature.EnableMultiNetwork {
-		cm.nadController, err = nad.NewNetAttachDefinitionController("network-controller-manager", cm, wf, nil)
+		cm.nadController, err = nad.NewZoneNADController("network-controller-manager", config.Default.Zone, cm, wf)
 		if err != nil {
 			return nil, err
 		}
 	}
+
 	return cm, nil
 }
 
@@ -290,7 +291,7 @@ func (cm *NetworkControllerManager) newCommonNetworkControllerInfo() (*ovn.Commo
 }
 
 // initDefaultNetworkController creates the controller for default network
-func (cm *NetworkControllerManager) initDefaultNetworkController(nadController *nad.NetAttachDefinitionController,
+func (cm *NetworkControllerManager) initDefaultNetworkController(nadController nad.NADController,
 	observManager *observability.Manager) error {
 	cnci, err := cm.newCommonNetworkControllerInfo()
 	if err != nil {
@@ -391,12 +392,6 @@ func (cm *NetworkControllerManager) Start(ctx context.Context) error {
 		metrics.GetConfigDurationRecorder().Run(cm.nbClient, cm.kube, 10, time.Second*5, cm.stopChan)
 	}
 	cm.podRecorder.Run(cm.sbClient, cm.stopChan)
-	// nadController is nil if multi-network is disabled
-	if cm.nadController != nil {
-		if err = cm.nadController.Start(); err != nil {
-			return fmt.Errorf("failed to start NAD Controller :%v", err)
-		}
-	}
 
 	var observabilityManager *observability.Manager
 	if config.OVNKubernetesFeature.EnableObservability {
@@ -414,6 +409,14 @@ func (cm *NetworkControllerManager) Start(ctx context.Context) error {
 	if err != nil {
 		return fmt.Errorf("failed to init default network controller: %v", err)
 	}
+
+	// nadController is nil if multi-network is disabled
+	if cm.nadController != nil {
+		if err = cm.nadController.Start(); err != nil {
+			return fmt.Errorf("failed to start NAD Controller :%v", err)
+		}
+	}
+
 	err = cm.defaultNetworkController.Start(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to start default network controller: %v", err)
