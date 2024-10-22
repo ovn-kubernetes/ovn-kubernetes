72b03f3eef3b286964d74e533ed8df2bb6354aa2
Dumitru Ceara <dceara@redhat.com>
2024-10-22 13:49:37 +0200

    bnc, udn: Add primary user defined network management port to cluster port group.
    Now that the cluster port group actually exists, add the mgmt port to it
    (similar to what happens on the default network).
    
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- go-controller/pkg/ovn/secondary_layer3_network_controller_test.go
+++ go-controller/pkg/ovn/secondary_layer3_network_controller_test.go
@@ -123,6 +123,7 @@ var _ = Describe("OVN Multi-Homed pod operations", func() {
 						&nbdb.LogicalRouterPort{
 							Name: fmt.Sprintf("rtos-%s_%s", netInfo.netName, nodeName),
 						},
+						newNetworkClusterPortGroup(getNetworkControllerName(netInfo.netName)),
 					)
 				}
 
@@ -187,6 +188,7 @@ var _ = Describe("OVN Multi-Homed pod operations", func() {
 					Expect(err).NotTo(HaveOccurred())
 					Expect(gwConfig.NextHops).NotTo(BeEmpty())
 					expectationOptions = append(expectationOptions, withGatewayConfig(gwConfig))
+					expectationOptions = append(expectationOptions, withClusterPortGroup())
 				}
 				Eventually(fakeOvn.nbClient).Should(
 					libovsdbtest.HaveData(
