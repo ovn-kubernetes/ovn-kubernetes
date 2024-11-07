ca2a8bf9ce55a7164f160a1c8263840e2fd74a06
Ram Lavi <ralavi@redhat.com>
2024-11-07 10:22:29 +0200

    tests, multi-homing: Expand test infra for multiple pod creation
    The current test infra pod creation function `dummyL2TestPod` creates
    the same testPod params each time. Introduce a podIdx input param to
    allow creating multiple pods that don't have colliding info (like MAC,
    IP, etc..). This will be used in future commits.
    
    Also, the test infra expect function `expectedLogicalSwitchesAndPorts`
    is changed to support multiple pods:
    - LogicalSwitch is updated on each pod loop iteration to update the
    pod's logical ports. However, logicalSwitch is added only after the pods
    loop, to prevent duplicate logical switches when not needed.
    - entities that need to be created only once, but rely on the pod data
    (subnets) are only added once per switch-node.
    
    These changes have no affect for existing tests, and will be relevant in
    next commits where tests with multiple pods will be introduced.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- go-controller/pkg/ovn/multihoming_test.go
+++ go-controller/pkg/ovn/multihoming_test.go
@@ -116,6 +116,7 @@ func (em *secondaryNetworkExpectationMachine) expectedLogicalSwitchesAndPorts(is
 		nodeslsps := make(map[string][]string)
 		acls := make(map[string][]string)
 		var switchName string
+		switchNodeMap := make(map[string]*nbdb.LogicalSwitch)
 		for _, pod := range em.pods {
 			podInfo, ok := pod.secondaryPodInfos[ocInfo.bnc.GetNetworkName()]
 			if !ok {
@@ -149,6 +150,7 @@ func (em *secondaryNetworkExpectationMachine) expectedLogicalSwitchesAndPorts(is
 					lsp.Options["requested-tnl-key"] = "1" // hardcode this for now.
 				}
 				data = append(data, lsp)
+
 				switch ocInfo.bnc.TopologyType() {
 				case ovntypes.Layer3Topology:
 					switchName = ocInfo.bnc.GetNetworkScopedName(pod.nodeName)
@@ -159,7 +161,7 @@ func (em *secondaryNetworkExpectationMachine) expectedLogicalSwitchesAndPorts(is
 					data = append(data, newExpectedSwitchToRouterPort(switchToRouterPortUUID, switchToRouterPortName, pod, ocInfo.bnc, nad))
 					nodeslsps[switchName] = append(nodeslsps[switchName], switchToRouterPortUUID)
 
-					if em.gatewayConfig != nil {
+					if _, exists := switchNodeMap[switchName]; !exists && em.gatewayConfig != nil {
 						mgmtPortName := managementPortName(switchName)
 						mgmtPortUUID := mgmtPortName + "-UUID"
 						mgmtPort := expectedManagementPort(mgmtPortName, managementIP.String())
@@ -176,7 +178,7 @@ func (em *secondaryNetworkExpectationMachine) expectedLogicalSwitchesAndPorts(is
 					switchName = ocInfo.bnc.GetNetworkScopedName(ovntypes.OVNLayer2Switch)
 					managementIP := managementPortIP(subnet)
 
-					if em.gatewayConfig != nil {
+					if _, exists := switchNodeMap[switchName]; !exists && em.gatewayConfig != nil {
 						// there are multiple mgmt ports in the cluster, thus the ports must be scoped with the node name
 						mgmtPortName := managementPortName(ocInfo.bnc.GetNetworkScopedName(nodeName))
 						mgmtPortUUID := mgmtPortName + "-UUID"
@@ -236,18 +238,7 @@ func (em *secondaryNetworkExpectationMachine) expectedLogicalSwitchesAndPorts(is
 				otherConfig = nil
 			}
 
-			data = append(data, &nbdb.LogicalSwitch{
-				UUID:  switchName + "-UUID",
-				Name:  switchName,
-				Ports: nodeslsps[switchName],
-				ExternalIDs: map[string]string{
-					ovntypes.NetworkExternalID:     ocInfo.bnc.GetNetworkName(),
-					ovntypes.NetworkRoleExternalID: util.GetUserDefinedNetworkRole(isPrimary),
-				},
-				OtherConfig: otherConfig,
-				ACLs:        acls[switchName],
-			})
-			if em.gatewayConfig != nil {
+			if _, exists := switchNodeMap[switchName]; !exists && em.gatewayConfig != nil {
 				if ocInfo.bnc.TopologyType() == ovntypes.Layer3Topology {
 					data = append(data, expectedGWEntities(pod.nodeName, subnets, ocInfo.bnc, *em.gatewayConfig)...)
 					data = append(data, expectedLayer3EgressEntities(ocInfo.bnc, *em.gatewayConfig, subnet)...)
@@ -255,7 +246,7 @@ func (em *secondaryNetworkExpectationMachine) expectedLogicalSwitchesAndPorts(is
 					data = append(data, expectedLayer2EgressEntities(ocInfo.bnc, *em.gatewayConfig, pod.nodeName)...)
 				}
 			}
-			if em.isInterconnectCluster && ocInfo.bnc.TopologyType() == ovntypes.Layer3Topology {
+			if _, exists := switchNodeMap[switchName]; !exists && em.isInterconnectCluster && ocInfo.bnc.TopologyType() == ovntypes.Layer3Topology {
 				transitSwitchName := ocInfo.bnc.GetNetworkName() + "_transit_switch"
 				data = append(data, &nbdb.LogicalSwitch{
 					UUID: transitSwitchName + "-UUID",
@@ -269,9 +260,23 @@ func (em *secondaryNetworkExpectationMachine) expectedLogicalSwitchesAndPorts(is
 					},
 				})
 			}
+			switchNodeMap[switchName] = &nbdb.LogicalSwitch{
+				UUID:  switchName + "-UUID",
+				Name:  switchName,
+				Ports: nodeslsps[switchName],
+				ExternalIDs: map[string]string{
+					ovntypes.NetworkExternalID:     ocInfo.bnc.GetNetworkName(),
+					ovntypes.NetworkRoleExternalID: util.GetUserDefinedNetworkRole(isPrimary),
+				},
+				OtherConfig: otherConfig,
+				ACLs:        acls[switchName],
+			}
 		}
-
+		for _, logicalSwitch := range switchNodeMap {
+			data = append(data, logicalSwitch)
+		}
 	}
+
 	return data
 }
 
