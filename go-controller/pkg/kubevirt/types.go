e5557cd4683f1127f97fcec265637ca73615c36f
Enrique Llorente <ellorent@redhat.com>
2024-11-25 11:28:37 +0100

    kubevirt: Add ipv6 default gw reconciler
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- go-controller/pkg/kubevirt/types.go
+++ go-controller/pkg/kubevirt/types.go
@@ -1,7 +1,9 @@
 package kubevirt
 
 import (
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/factory"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
 )
 
 const (
@@ -12,3 +14,19 @@ const (
 	NamespaceExternalIDsKey      = "k8s.ovn.org/namespace"
 	VirtualMachineExternalIDsKey = "k8s.ovn.org/vm"
 )
+
+// DefaultGatewayReconciler implements a pair of Reconcile functions for
+// ipv4 and ipv6 to update VM's default gw config after live migration
+type DefaultGatewayReconciler struct {
+	watchFactory  *factory.WatchFactory
+	netInfo       util.NetInfo
+	interfaceName string
+}
+
+func NewDefaultGatewayReconciler(watchFactory *factory.WatchFactory, netInfo util.NetInfo, interfaceName string) *DefaultGatewayReconciler {
+	return &DefaultGatewayReconciler{
+		watchFactory:  watchFactory,
+		netInfo:       netInfo,
+		interfaceName: interfaceName,
+	}
+}
