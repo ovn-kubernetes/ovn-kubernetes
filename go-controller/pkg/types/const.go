b13fd232a4999fca6181d30ac82b50bba6fbe3d9
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2025-02-17 18:08:43 +0000

    Move gateway/management port shared types
    These are types that are used by both the gateway and the management
    port code. Export them as the plan is to have management port on its own
    package.
    
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- go-controller/pkg/types/const.go
+++ go-controller/pkg/types/const.go
@@ -264,4 +264,22 @@ const (
 	// entry for the gateway routers. After this time, the entry is removed and
 	// may be refreshed with a new ARP request.
 	GRMACBindingAgeThreshold = "300"
+
+	// OVNKubeITPMark is the fwmark used for host->ITP=local svc traffic. Note that the fwmark is not a part
+	// of the packet, but just stored by kernel in its memory to track/filter packet. Hence fwmark is lost as
+	// soon as packet exits the host.
+	OVNKubeITPMark = "0x1745ec" // constant itp(174)-service(5ec)
+
+	// "mgmtport-no-snat-nodeports" is a set containing protocol / nodePort tuples
+	// indicating traffic that should not be SNATted when passing through the
+	// management port because it is addressed to an `externalTrafficPolicy: Local`
+	// NodePort.
+	NFTMgmtPortNoSNATNodePorts = "mgmtport-no-snat-nodeports"
+
+	// "mgmtport-no-snat-services-v4" and "mgmtport-no-snat-services-v6" are sets
+	// containing loadBalancerIP / protocol / port tuples indicating traffic that
+	// should not be SNATted when passing through the management port because it is
+	// addressed to an `externalTrafficPolicy: Local` load balancer IP.
+	NFTMgmtPortNoSNATServicesV4 = "mgmtport-no-snat-services-v4"
+	NFTMgmtPortNoSNATServicesV6 = "mgmtport-no-snat-services-v6"
 )
