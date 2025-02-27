jcaamano/ovn-kubernetes-downstream

Make EIP controllers aware of route advertisements
Make cluster manager, zone and node EIP controllers aware of route
advertisements.

An egress IP can be either local to a node (the node is directly
connected to the egress IP network through the primary or a secondary
interface) or advertised through a node.

Cluster manager EIP controller checks if an egress IP that is not local
to any node can be instead advertised on any node and if so, assigns it to
that node. For an egress IP to be advertised on a node, a route
advertisement configured to advertise egress IPs must select a network
that is serving any of the namespaces selected by the egress IP. If
different networks serve the namespaces selected by an egress IP, all
those newtworks must be configured to advertise egress IPs on at least
one common node for the egress IP to be assigned. To be able to reconcile
updates correctly, the controller listens to network and namespace events.

Zone and node controllers require minor changes to merely treat
advertised egress IPs as if they were local to the primary network
interface.

Egress IP utility methods are reorganized in util/egressip/util.go

Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

go-controller/pkg/clustermanager/egressip_event_handler.go
@@ -67,6 +67,9 @@ func (h *egressIPClusterControllerEventHandler) AddResource(obj interface{}, fro
 	case factory.CloudPrivateIPConfigType:
 		cloudPrivateIPConfig := obj.(*ocpcloudnetworkapi.CloudPrivateIPConfig)
 		return h.eIPC.reconcileCloudPrivateIPConfig(nil, cloudPrivateIPConfig)
+	case factory.EgressIPNamespaceType:
+		namespace := obj.(*v1.Namespace)
+		return h.eIPC.reconcileNamespace(nil, namespace)
 	default:
 		return fmt.Errorf("no add function for object type %s", h.objType)
 	}
@@ -159,6 +162,10 @@ func (h *egressIPClusterControllerEventHandler) UpdateResource(oldObj, newObj in
 		oldCloudPrivateIPConfig := oldObj.(*ocpcloudnetworkapi.CloudPrivateIPConfig)
 		newCloudPrivateIPConfig := newObj.(*ocpcloudnetworkapi.CloudPrivateIPConfig)
 		return h.eIPC.reconcileCloudPrivateIPConfig(oldCloudPrivateIPConfig, newCloudPrivateIPConfig)
+	case factory.EgressIPNamespaceType:
+		oldNamespace := oldObj.(*v1.Namespace)
+		newNamespace := newObj.(*v1.Namespace)
+		return h.eIPC.reconcileNamespace(oldNamespace, newNamespace)
 	default:
 		return fmt.Errorf("no update function for object type %s", h.objType)
 	}
@@ -190,6 +197,9 @@ func (h *egressIPClusterControllerEventHandler) DeleteResource(obj, cachedObj in
 	case factory.CloudPrivateIPConfigType:
 		cloudPrivateIPConfig := obj.(*ocpcloudnetworkapi.CloudPrivateIPConfig)
 		return h.eIPC.reconcileCloudPrivateIPConfig(cloudPrivateIPConfig, nil)
+	case factory.EgressIPNamespaceType:
+		oldNamespace := obj.(*v1.Namespace)
+		return h.eIPC.reconcileNamespace(oldNamespace, nil)
 	default:
 		return fmt.Errorf("no delete function for object type %s", h.objType)
 	}
@@ -209,7 +219,8 @@ func (h *egressIPClusterControllerEventHandler) SyncFunc(objs []interface{}) err
 			syncFunc = h.eIPC.initEgressNodeReachability
 		case factory.CloudPrivateIPConfigType:
 			syncFunc = h.eIPC.syncCloudPrivateIPConfigs
-
+		case factory.EgressIPNamespaceType:
+			syncFunc = nil
 		default:
 			return fmt.Errorf("no sync function for object type %s", h.objType)
 		}
@@ -239,6 +250,8 @@ func (h *egressIPClusterControllerEventHandler) GetResourceFromInformerCache(key
 		obj, err = h.eIPC.watchFactory.GetCloudPrivateIPConfig(name)
 	case factory.EgressIPType:
 		obj, err = h.eIPC.watchFactory.GetEgressIP(name)
+	case factory.EgressIPNamespaceType:
+		obj, err = h.eIPC.watchFactory.GetNamespace(name)
 
 	default:
 		err = fmt.Errorf("object type %s not supported, cannot retrieve it from informers cache",

ovn-kubernetes/ovn-kubernetes

go-controller/pkg/clustermanager/egressip_event_handler.go
@@ -73,6 +73,9 @@ func (h *egressIPClusterControllerEventHandler) AddResource(obj interface{}, _ b
 	case factory.CloudPrivateIPConfigType:
 		cloudPrivateIPConfig := obj.(*ocpcloudnetworkapi.CloudPrivateIPConfig)
 		return h.eIPC.reconcileCloudPrivateIPConfig(nil, cloudPrivateIPConfig)
+	case factory.EgressIPNamespaceType:
+		namespace := obj.(*corev1.Namespace)
+		return h.eIPC.reconcileNamespace(nil, namespace)
 	default:
 		return fmt.Errorf("no add function for object type %s", h.objType)
 	}
@@ -165,6 +168,10 @@ func (h *egressIPClusterControllerEventHandler) UpdateResource(oldObj, newObj in
 		oldCloudPrivateIPConfig := oldObj.(*ocpcloudnetworkapi.CloudPrivateIPConfig)
 		newCloudPrivateIPConfig := newObj.(*ocpcloudnetworkapi.CloudPrivateIPConfig)
 		return h.eIPC.reconcileCloudPrivateIPConfig(oldCloudPrivateIPConfig, newCloudPrivateIPConfig)
+	case factory.EgressIPNamespaceType:
+		oldNamespace := oldObj.(*corev1.Namespace)
+		newNamespace := newObj.(*corev1.Namespace)
+		return h.eIPC.reconcileNamespace(oldNamespace, newNamespace)
 	default:
 		return fmt.Errorf("no update function for object type %s", h.objType)
 	}
@@ -196,6 +203,9 @@ func (h *egressIPClusterControllerEventHandler) DeleteResource(obj, _ interface{
 	case factory.CloudPrivateIPConfigType:
 		cloudPrivateIPConfig := obj.(*ocpcloudnetworkapi.CloudPrivateIPConfig)
 		return h.eIPC.reconcileCloudPrivateIPConfig(cloudPrivateIPConfig, nil)
+	case factory.EgressIPNamespaceType:
+		oldNamespace := obj.(*corev1.Namespace)
+		return h.eIPC.reconcileNamespace(oldNamespace, nil)
 	default:
 		return fmt.Errorf("no delete function for object type %s", h.objType)
 	}
@@ -215,7 +225,8 @@ func (h *egressIPClusterControllerEventHandler) SyncFunc(objs []interface{}) err
 			syncFunc = h.eIPC.initEgressNodeReachability
 		case factory.CloudPrivateIPConfigType:
 			syncFunc = h.eIPC.syncCloudPrivateIPConfigs
-
+		case factory.EgressIPNamespaceType:
+			syncFunc = nil
 		default:
 			return fmt.Errorf("no sync function for object type %s", h.objType)
 		}
@@ -245,6 +256,8 @@ func (h *egressIPClusterControllerEventHandler) GetResourceFromInformerCache(key
 		obj, err = h.eIPC.watchFactory.GetCloudPrivateIPConfig(name)
 	case factory.EgressIPType:
 		obj, err = h.eIPC.watchFactory.GetEgressIP(name)
+	case factory.EgressIPNamespaceType:
+		obj, err = h.eIPC.watchFactory.GetNamespace(name)
 
 	default:
 		err = fmt.Errorf("object type %s not supported, cannot retrieve it from informers cache",