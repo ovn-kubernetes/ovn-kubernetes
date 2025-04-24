196aab5300629da08e74a9a9653f7e632e03c5e4
Periyasamy Palanisamy <pepalani@redhat.com>
2025-04-24 14:22:25 +0200

    Add IPsec tunnel state metric
    This commit adds ovnkube_controller_ipsec_tunnel_state metric which reflects
    IKE SA establishment state for all the configured IPsec tunnels via OVN.
    When all tunnels has IKE SAs established, then metric is set to 1, otherwise
    it will be reset to 0. The implementation is done by listening to xfrm netlink
    change events for xfrm state and policy.
    
    Signed-off-by: Periyasamy Palanisamy <pepalani@redhat.com>
    (cherry picked from commit e499f32c05696cbe726657b7318d663fd40b54a7)

---
+++ go-controller/pkg/metrics/ipsec_test.go
@@ -0,0 +1,223 @@
+package metrics
+
+import (
+	"fmt"
+	"net"
+	"sync"
+	"time"
+
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+	"github.com/vishvananda/netlink"
+
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/metrics/mocks"
+)
+
+type fakeIPsecClient struct {
+	output clientOutput
+	mutex  *sync.Mutex
+}
+
+func NewFakeIPsecClient(data clientOutput) fakeIPsecClient {
+	return fakeIPsecClient{output: data, mutex: &sync.Mutex{}}
+}
+
+func (c *fakeIPsecClient) FakeCall(...string) (string, string, error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	return c.output.stdout, c.output.stderr, c.output.err
+}
+
+func (c *fakeIPsecClient) ChangeOutput(newOutput clientOutput) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.output = newOutput
+}
+
+var _ = ginkgo.Describe("IPsec metrics", func() {
+	var (
+		localIP                    = "10.89.0.2"
+		geneveTunnelName1          = "ovn-8ebfff-0"
+		remoteIP1                  = "10.89.0.3"
+		geneveTunnelName2          = "ovn-e9845d-0"
+		remoteIP2                  = "10.89.0.4"
+		stopChan                   chan struct{}
+		wg                         *sync.WaitGroup
+		mockIPsecTunnelStateMetric *mocks.GaugeMock
+	)
+
+	ginkgo.BeforeEach(func() {
+		stopChan = make(chan struct{})
+		wg = &sync.WaitGroup{}
+		mockIPsecTunnelStateMetric = mocks.NewGaugeMock()
+		metricIPsecTunnelState = mockIPsecTunnelStateMetric
+	})
+
+	ginkgo.AfterEach(func() {
+		ginkgo.By("clean up resources for the test")
+		close(stopChan)
+		wg.Wait()
+		err := netlink.XfrmStateDel(getBaseState())
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	})
+
+	ginkgo.Context("Tunnel state", func() {
+		ginkgo.It("when geneve tunnels are present with IKE Child SAs established", func() {
+			ovsVsCtlCmdOutput := clientOutput{
+				stdout: fmt.Sprintf(`{"data":[["%[1]s","up","up",["map",[["csum","true"],["key","flow"],
+				["local_ip","%[2]s"],["remote_ip","%[3]s"],["remote_name","8ebfffbe-3cac-4a67-8f42-c74b708f8cc6"]]]],
+				["%[4]s","up","up",["map",[["csum","true"],["key","flow"],["local_ip","%[2]s"],["remote_ip","%[5]s"],
+				["remote_name","e9845dc0-283f-4ea0-a9fa-4418bb6708c4"]]]]], "headings":["name","admin_state",
+				"link_state","options"]}`, geneveTunnelName1, localIP, remoteIP1, geneveTunnelName2, remoteIP2),
+				stderr: "",
+				err:    nil,
+			}
+			ovsVsctl := NewFakeOVSClientWithSameOutput(ovsVsCtlCmdOutput)
+			ipsecCmdOutput := clientOutput{
+				stdout: `000 #13: "ovn-8ebfff-0-in-1":500 STATE_V2_ESTABLISHED_CHILD_SA (established Child SA); REKEY in 23543s; REPLACE in 24315s; newest; eroute owner; IKE SA #16; idle;
+                             000 #16: "ovn-8ebfff-0-in-1":500 STATE_V2_ESTABLISHED_IKE_SA (established IKE SA); REKEY in 23975s; REPLACE in 24736s; newest; idle;
+                             000 #11: "ovn-8ebfff-0-out-1":500 STATE_V2_ESTABLISHED_CHILD_SA (established Child SA); REKEY in 23942s; REPLACE in 24212s; newest; eroute owner; IKE SA #16; idle;
+                             000 #14: "ovn-e9845d-0-in-1":500 STATE_V2_ESTABLISHED_IKE_SA (established IKE SA); REKEY in 23575s; REPLACE in 24525s; newest; idle;
+                             000 #15: "ovn-e9845d-0-in-1":500 STATE_V2_ESTABLISHED_CHILD_SA (established Child SA); REKEY in 23873s; REPLACE in 24623s; newest; eroute owner; IKE SA #14; idle;
+                             000 #12: "ovn-e9845d-0-out-1":500 STATE_V2_ESTABLISHED_CHILD_SA (established Child SA); REKEY in 24034s; REPLACE in 24304s; newest; eroute owner; IKE SA #14; idle;`,
+				stderr: "",
+				err:    nil,
+			}
+			ipsec := NewFakeIPsecClient(ipsecCmdOutput)
+			MonitorIPsecTunnelsState(stopChan, wg, ovsVsctl.FakeCall, ipsec.FakeCall)
+			time.Sleep(2 * time.Second) // Allow some time for the goroutine to start
+			err := netlink.XfrmStateAdd(getBaseState())
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			gomega.Eventually(func() int {
+				return int(mockIPsecTunnelStateMetric.GetValue())
+			}).WithTimeout(20 * time.Second).Should(gomega.Equal(1))
+		})
+		ginkgo.It("when geneve tunnels are present with IKE Child SAs not established for a tunnel", func() {
+			ovsVsCtlCmdOutput := clientOutput{
+				stdout: fmt.Sprintf(`{"data":[["%[1]s","up","up",["map",[["csum","true"],["key","flow"],
+				["local_ip","%[2]s"],["remote_ip","%[3]s"],["remote_name","8ebfffbe-3cac-4a67-8f42-c74b708f8cc6"]]]],
+				["%[4]s","up","up",["map",[["csum","true"],["key","flow"],["local_ip","%[2]s"],["remote_ip","%[5]s"],
+				["remote_name","e9845dc0-283f-4ea0-a9fa-4418bb6708c4"]]]]], "headings":["name","admin_state",
+				"link_state","options"]}`, geneveTunnelName1, localIP, remoteIP1, geneveTunnelName2, remoteIP2),
+				stderr: "",
+				err:    nil,
+			}
+			ovsVsctl := NewFakeOVSClientWithSameOutput(ovsVsCtlCmdOutput)
+			ipsecCmdOutput := clientOutput{
+				stdout: `000 #13: "ovn-8ebfff-0-in-1":500 STATE_V2_ESTABLISHED_CHILD_SA (established Child SA); REKEY in 23543s; REPLACE in 24315s; newest; eroute owner; IKE SA #16; idle;
+                             000 #16: "ovn-8ebfff-0-in-1":500 STATE_V2_ESTABLISHED_IKE_SA (established IKE SA); REKEY in 23975s; REPLACE in 24736s; newest; idle;
+                             000 #14: "ovn-e9845d-0-in-1":500 STATE_V2_ESTABLISHED_IKE_SA (established IKE SA); REKEY in 23575s; REPLACE in 24525s; newest; idle;
+                             000 #15: "ovn-e9845d-0-in-1":500 STATE_V2_ESTABLISHED_CHILD_SA (established Child SA); REKEY in 23873s; REPLACE in 24623s; newest; eroute owner; IKE SA #14; idle;
+                             000 #12: "ovn-e9845d-0-out-1":500 STATE_V2_ESTABLISHED_CHILD_SA (established Child SA); REKEY in 24034s; REPLACE in 24304s; newest; eroute owner; IKE SA #14; idle;`,
+				stderr: "",
+				err:    nil,
+			}
+			ipsec := NewFakeIPsecClient(ipsecCmdOutput)
+			MonitorIPsecTunnelsState(stopChan, wg, ovsVsctl.FakeCall, ipsec.FakeCall)
+			time.Sleep(2 * time.Second) // Allow some time for the goroutine to start
+			err := netlink.XfrmStateAdd(getBaseState())
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			gomega.Eventually(func() int {
+				return int(mockIPsecTunnelStateMetric.GetValue())
+			}).WithTimeout(20 * time.Second).Should(gomega.Equal(0))
+		})
+
+		ginkgo.It("check IKE Child SA establishment when geneve tunnel interface flapping scenario", func() {
+			// Test no IPsec tunnel metrics when both Geneve tunnels are down.
+			ovsVsCtlCmdOutput := clientOutput{
+				stdout: fmt.Sprintf(`{"data":[["%[1]s","up","down",["map",[["csum","true"],["key","flow"],
+				["local_ip","%[2]s"],["remote_ip","%[3]s"],["remote_name","8ebfffbe-3cac-4a67-8f42-c74b708f8cc6"]]]],
+				["%[4]s","up","down",["map",[["csum","true"],["key","flow"],["local_ip","%[2]s"],["remote_ip","%[5]s"],
+				["remote_name","e9845dc0-283f-4ea0-a9fa-4418bb6708c4"]]]]], "headings":["name","admin_state",
+				"link_state","options"]}`, geneveTunnelName1, localIP, remoteIP1, geneveTunnelName2, remoteIP2),
+				stderr: "",
+				err:    nil,
+			}
+			ovsVsctl := NewFakeOVSClientWithSameOutput(ovsVsCtlCmdOutput)
+			ipsecCmdOutput := clientOutput{stdout: "", stderr: "", err: nil}
+			ipsec := NewFakeIPsecClient(ipsecCmdOutput)
+			MonitorIPsecTunnelsState(stopChan, wg, ovsVsctl.FakeCall, ipsec.FakeCall)
+			time.Sleep(2 * time.Second) // Allow some time for the goroutine to start
+			state := getBaseState()
+			err := netlink.XfrmStateAdd(state)
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			gomega.Consistently(func() int {
+				return int(mockIPsecTunnelStateMetric.GetValue())
+			}).WithTimeout(5 * time.Second).Should(gomega.Equal(0))
+			// Test correspoding IPsec tunnel metrics when one of the Geneve tunnels is down.
+			ovsVsCtlCmdOutput = clientOutput{
+				stdout: fmt.Sprintf(`{"data":[["%[1]s","up","up",["map",[["csum","true"],["key","flow"],
+				["local_ip","%[2]s"],["remote_ip","%[3]s"],["remote_name","8ebfffbe-3cac-4a67-8f42-c74b708f8cc6"]]]],
+				["%[4]s","up","down",["map",[["csum","true"],["key","flow"],["local_ip","%[2]s"],["remote_ip","%[5]s"],
+				["remote_name","e9845dc0-283f-4ea0-a9fa-4418bb6708c4"]]]]], "headings":["name","admin_state",
+				"link_state","options"]}`, geneveTunnelName1, localIP, remoteIP1, geneveTunnelName2, remoteIP2),
+				stderr: "",
+				err:    nil,
+			}
+			ovsVsctl.ChangeOutput(ovsVsCtlCmdOutput)
+			ipsecCmdOutput = clientOutput{
+				stdout: `000 #13: "ovn-8ebfff-0-in-1":500 STATE_V2_ESTABLISHED_CHILD_SA (established Child SA); REKEY in 23543s; REPLACE in 24315s; newest; eroute owner; IKE SA #16; idle;
+                             000 #16: "ovn-8ebfff-0-in-1":500 STATE_V2_ESTABLISHED_IKE_SA (established IKE SA); REKEY in 23975s; REPLACE in 24736s; newest; idle;
+                             000 #11: "ovn-8ebfff-0-out-1":500 STATE_V2_ESTABLISHED_CHILD_SA (established Child SA); REKEY in 23942s; REPLACE in 24212s; newest; eroute owner; IKE SA #16; idle;`,
+				stderr: "",
+				err:    nil,
+			}
+			ipsec.ChangeOutput(ipsecCmdOutput)
+			err = netlink.XfrmStateDel(state)
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			gomega.Consistently(func() int {
+				return int(mockIPsecTunnelStateMetric.GetValue())
+			}).WithTimeout(5 * time.Second).Should(gomega.Equal(0))
+			// Test correspoding IPsec tunnel metrics when Geneve tunnels are up.
+			ovsVsCtlCmdOutput = clientOutput{
+				stdout: fmt.Sprintf(`{"data":[["%[1]s","up","up",["map",[["csum","true"],["key","flow"],
+				["local_ip","%[2]s"],["remote_ip","%[3]s"],["remote_name","8ebfffbe-3cac-4a67-8f42-c74b708f8cc6"]]]],
+				["%[4]s","up","up",["map",[["csum","true"],["key","flow"],["local_ip","%[2]s"],["remote_ip","%[5]s"],
+				["remote_name","e9845dc0-283f-4ea0-a9fa-4418bb6708c4"]]]]], "headings":["name","admin_state",
+				"link_state","options"]}`, geneveTunnelName1, localIP, remoteIP1, geneveTunnelName2, remoteIP2),
+				stderr: "",
+				err:    nil,
+			}
+			ovsVsctl.ChangeOutput(ovsVsCtlCmdOutput)
+			ipsecCmdOutput = clientOutput{
+				stdout: `000 #13: "ovn-8ebfff-0-in-1":500 STATE_V2_ESTABLISHED_CHILD_SA (established Child SA); REKEY in 23543s; REPLACE in 24315s; newest; eroute owner; IKE SA #16; idle;
+                             000 #16: "ovn-8ebfff-0-in-1":500 STATE_V2_ESTABLISHED_IKE_SA (established IKE SA); REKEY in 23975s; REPLACE in 24736s; newest; idle;
+                             000 #11: "ovn-8ebfff-0-out-1":500 STATE_V2_ESTABLISHED_CHILD_SA (established Child SA); REKEY in 23942s; REPLACE in 24212s; newest; eroute owner; IKE SA #16; idle;
+                             000 #14: "ovn-e9845d-0-in-1":500 STATE_V2_ESTABLISHED_IKE_SA (established IKE SA); REKEY in 23575s; REPLACE in 24525s; newest; idle;
+                             000 #15: "ovn-e9845d-0-in-1":500 STATE_V2_ESTABLISHED_CHILD_SA (established Child SA); REKEY in 23873s; REPLACE in 24623s; newest; eroute owner; IKE SA #14; idle;
+                             000 #12: "ovn-e9845d-0-out-1":500 STATE_V2_ESTABLISHED_CHILD_SA (established Child SA); REKEY in 24034s; REPLACE in 24304s; newest; eroute owner; IKE SA #14; idle;`,
+				stderr: "",
+				err:    nil,
+			}
+			ipsec.ChangeOutput(ipsecCmdOutput)
+			err = netlink.XfrmStateAdd(state)
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			gomega.Eventually(func() int {
+				return int(mockIPsecTunnelStateMetric.GetValue())
+			}).WithTimeout(20 * time.Second).Should(gomega.Equal(1))
+		})
+	})
+})
+
+func getBaseState() *netlink.XfrmState {
+	return &netlink.XfrmState{
+		// Force 4 byte notation for the IPv4 addresses
+		Src:   net.ParseIP("127.0.0.1").To4(),
+		Dst:   net.ParseIP("127.0.0.2").To4(),
+		Proto: netlink.XFRM_PROTO_ESP,
+		Mode:  netlink.XFRM_MODE_TUNNEL,
+		Spi:   1,
+		Auth: &netlink.XfrmStateAlgo{
+			Name: "hmac(sha256)",
+			Key:  []byte("abcdefghijklmnopqrstuvwzyzABCDEF"),
+		},
+		Crypt: &netlink.XfrmStateAlgo{
+			Name: "cbc(aes)",
+			Key:  []byte("abcdefghijklmnopqrstuvwzyzABCDEF"),
+		},
+		Mark: &netlink.XfrmMark{
+			Value: 0x12340000,
+			Mask:  0xffff0000,
+		},
+	}
+}
