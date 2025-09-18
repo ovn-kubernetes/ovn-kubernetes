ead5b65fa56fb5a9f3d2d2e5e5e41f1e7884c4f6
zhaozhanqi <zzhao@redhat.com>
2025-09-18 13:28:12 +0000

    for LGW with ETP=Local have different error code for l3 and l2
    For ipv6 dualstack cluster + LGW +  etp=local
    
    1.UDN pod to a different node nodeport service in same UDN network should work
    2. UDN pod to a different node nodeport service in different UDN network should work
    3. Default network pod to different node nodeport service in UDN network should work
    
    Layer3:  curl ipv4:nodePort show error code 56 and ipv6 show errorcode 28
    
      I0919 11:40:16.061851 3313931 route_advertisements.go:808] Attempting connectivity from pod: bgp-network-isolation-4612/pod-1-ovn-control-plane-net-bgp-udn-layer3-network-a -> 172.18.0.2:30239
      I0919 11:40:16.061940 3313931 builder.go:121] Running '/usr/local/bin/kubectl --server=https://127.0.0.1:46363 --kubeconfig=/root/ovn.conf --namespace=bgp-network-isolation-4612 exec pod-1-ovn-control-plane-net-bgp-udn-layer3-network-a -- /bin/sh -x -c curl -g -q -s --max-time 2 --insecure 172.18.0.2:30239'
      I0919 11:40:16.364346 3313931 builder.go:135] rc: 56
      I0919 11:40:17.187795 3313931 route_advertisements.go:808] Attempting connectivity from pod: bgp-network-isolation-4612/pod-1-ovn-control-plane-net-bgp-udn-layer3-network-a -> [fc00:f853:ccd:e793::2]:30239
      I0919 11:40:17.187912 3313931 builder.go:121] Running '/usr/local/bin/kubectl --server=https://127.0.0.1:46363 --kubeconfig=/root/ovn.conf --namespace=bgp-network-isolation-4612 exec pod-1-ovn-control-plane-net-bgp-udn-layer3-network-a -- /bin/sh -x -c curl -g -q -s --max-time 2 --insecure [fc00:f853:ccd:e793::2]:30239'
      I0919 11:40:19.289747 3313931 builder.go:135] rc: 28
    
    Layer2:  curl ipv4: nodePort works but ipv6 show error 28
    
      I0919 11:34:20.987943 3301947 builder.go:121] Running '/usr/local/bin/kubectl --server=https://127.0.0.1:46363 --kubeconfig=/root/ovn.conf --namespace=bgp-network-isolation-3411 exec pod-1-ovn-control-plane-net-bgp-udn-layer2-network-a -- /bin/sh -x -c curl -g -q -s --max-time 2 --insecure 172.18.0.2:31511'
      I0919 11:34:21.163891 3301947 builder.go:146] stderr: "+ curl -g -q -s --max-time 2 --insecure 172.18.0.2:31511\n"
      I0919 11:34:21.163949 3301947 builder.go:147] stdout: "NOW: 2025-09-19 11:34:21.157593953 +0000 UTC m=+21.524517109"
      I0919 11:34:21.163973 3301947 route_advertisements.go:827] Connectivity check successful:'bgp-network-isolation-3411/pod-1-ovn-control-plane-net-bgp-udn-layer2-network-a' -> 172.18.0.2:31511
      I0919 11:34:21.168011 3301947 route_advertisements.go:808] Attempting connectivity from pod: bgp-network-isolation-3411/pod-1-ovn-control-plane-net-bgp-udn-layer2-network-a -> [fc00:f853:ccd:e793::2]:31511
      I0919 11:34:21.168093 3301947 builder.go:121] Running '/usr/local/bin/kubectl --server=https://127.0.0.1:46363 --kubeconfig=/root/ovn.conf --namespace=bgp-network-isolation-3411 exec pod-1-ovn-control-plane-net-bgp-udn-layer2-network-a -- /bin/sh -x -c curl -g -q -s --max-time 2 --insecure [fc00:f853:ccd:e793::2]:31511'
      I0919 11:34:23.277432 3301947 builder.go:135] rc: 28
    
    However for ipv6 single stack + LGW + etp=local
    
    Layer3: ipv6 return 56
    
       BGP: isolation between advertised networks Layer3 connectivity between networks [It] [ETP=LOCAL]UDN pod to a different node nodeport service in same UDN network should work
       expected connectivity check to contain "28", got "\n+ curl -g -q -s --max-time 2 --insecure '[fc00:f853:ccd:e793::4]:31267'\ncommand terminated with exit code 56\n"
    
    layer2: ipv6 works
             + curl -g -q -s --max-time 2 --insecure '[fc00:f853:ccd:e793::4]:30671'
    
          {
              s: "expected connectivity check to return error(true), got <nil>, output NOW: 2025-09-19 11:24:10.715276089 +0000 UTC m=+22.755683857\n+ curl -g -q -s --max-time 2 --insecure '[fc00:f853:ccd:e793::4]:30671'\n",
    
    Signed-off-by: zhaozhanqi <zzhao@redhat.com>

--- test/e2e/route_advertisements.go
+++ test/e2e/route_advertisements.go
@@ -1169,7 +1169,7 @@ var _ = ginkgo.DescribeTableSubtree("BGP: isolation between advertised networks"
 					nodePort := svcNodePortNetB.Spec.Ports[0].NodePort
 					out := curlConnectionTimeoutCode
 					errBool := true
-					if ipFamily == utilnet.IPv6 && cudnATemplate.Spec.Network.Topology == udnv1.NetworkTopologyLayer2 {
+					if ipFamily == utilnet.IPv6 && cudnATemplate.Spec.Network.Topology == udnv1.NetworkTopologyLayer2 && !IsGatewayModeLocal(f.ClientSet) {
 						// For Layer2 networks, we have these flows we add on breth0:
 						// cookie=0xdeff105, duration=173.245s, table=1, n_packets=0, n_bytes=0, idle_age=173, priority=14,icmp6,icmp_type=134 actions=FLOOD
 						// cookie=0xdeff105, duration=173.245s, table=1, n_packets=8, n_bytes=640, idle_age=4, priority=14,icmp6,icmp_type=136 actions=FLOOD
@@ -1228,7 +1228,27 @@ var _ = ginkgo.DescribeTableSubtree("BGP: isolation between advertised networks"
 						nodeIP = nodeIPv6
 					}
 					nodePortA := svcNodePortETPLocalNetA.Spec.Ports[0].NodePort
-					return clientPod.Name, clientPod.Namespace, net.JoinHostPort(nodeIP, fmt.Sprint(nodePortA)), "", false
+
+					out := ""
+					errBool := false
+					// FIXME https://github.com/ovn-kubernetes/ovn-kubernetes/issues/5531
+					// For LGW mode, nodePort service with ETP=local is not working for UDN networks
+					if IsGatewayModeLocal(f.ClientSet) {
+						out = curlConnectionTimeoutCode
+						errBool = true
+
+						if cudnATemplate.Spec.Network.Topology == udnv1.NetworkTopologyLayer3 && ipFamily == utilnet.IPv4 {
+							out = "56"
+							errBool = true
+						}
+						if cudnATemplate.Spec.Network.Topology == udnv1.NetworkTopologyLayer2 && ipFamily == utilnet.IPv4 {
+							out = ""
+							errBool = false
+						}
+
+					}
+
+					return clientPod.Name, clientPod.Namespace, net.JoinHostPort(nodeIP, fmt.Sprint(nodePortA)), out, errBool
 				}),
 
 			ginkgo.Entry("[ETP=LOCAL] UDN pod to the same node nodeport service in different UDN network should not work",
@@ -1244,7 +1264,7 @@ var _ = ginkgo.DescribeTableSubtree("BGP: isolation between advertised networks"
 					nodePortA := svcNodePortETPLocalNetA.Spec.Ports[0].NodePort
 					out := curlConnectionTimeoutCode
 					errBool := true
-					if ipFamily == utilnet.IPv6 && cudnATemplate.Spec.Network.Topology == udnv1.NetworkTopologyLayer2 {
+					if ipFamily == utilnet.IPv6 && cudnATemplate.Spec.Network.Topology == udnv1.NetworkTopologyLayer2 && !IsGatewayModeLocal(f.ClientSet) {
 						// For Layer2 networks, we have these flows we add on breth0:
 						// cookie=0xdeff105, duration=173.245s, table=1, n_packets=0, n_bytes=0, idle_age=173, priority=14,icmp6,icmp_type=134 actions=FLOOD
 						// cookie=0xdeff105, duration=173.245s, table=1, n_packets=8, n_bytes=640, idle_age=4, priority=14,icmp6,icmp_type=136 actions=FLOOD
@@ -1255,6 +1275,7 @@ var _ = ginkgo.DescribeTableSubtree("BGP: isolation between advertised networks"
 						// See the tcpdump on the issue: https://github.com/ovn-kubernetes/ovn-kubernetes/issues/5410 for more details.
 						out = ""
 						errBool = false
+
 					}
 					return clientPod.Name, clientPod.Namespace, net.JoinHostPort(nodeIP, fmt.Sprint(nodePortA)), out, errBool
 				}),
@@ -1271,6 +1292,22 @@ var _ = ginkgo.DescribeTableSubtree("BGP: isolation between advertised networks"
 					nodePortA := svcNodePortETPLocalNetA.Spec.Ports[0].NodePort
 					out := ""
 					errBool := false
+
+					// FIXME https://github.com/ovn-kubernetes/ovn-kubernetes/issues/5531
+					// For LGW mode, nodePort service with ETP=local is not working for UDN networks
+					if IsGatewayModeLocal(f.ClientSet) {
+						out = curlConnectionTimeoutCode
+						errBool = true
+						if cudnATemplate.Spec.Network.Topology == udnv1.NetworkTopologyLayer3 && ipFamily == utilnet.IPv4 {
+							out = "56"
+							errBool = true
+						}
+						if cudnATemplate.Spec.Network.Topology == udnv1.NetworkTopologyLayer2 && ipFamily == utilnet.IPv4 {
+							out = ""
+							errBool = false
+						}
+
+					}
 					return clientPod.Name, clientPod.Namespace, net.JoinHostPort(nodeIP, fmt.Sprint(nodePortA)), out, errBool
 				}),
 			ginkgo.Entry("[ETP=LOCAL] UDN pod to the same node nodeport service in default network should not work",
@@ -1297,7 +1334,9 @@ var _ = ginkgo.DescribeTableSubtree("BGP: isolation between advertised networks"
 						nodeIP = nodeIPv6
 					}
 					nodePortB := svcNodePortETPLocalDefault.Spec.Ports[0].NodePort
-					return clientPod.Name, clientPod.Namespace, net.JoinHostPort(nodeIP, fmt.Sprint(nodePortB)), "", false
+					out := ""
+					errBool := false
+					return clientPod.Name, clientPod.Namespace, net.JoinHostPort(nodeIP, fmt.Sprint(nodePortB)), out, errBool
 				}),
 			ginkgo.Entry("[ETP=LOCAL] Default network pod to same node nodeport service in UDN network should not work",
 				func(ipFamily utilnet.IPFamily) (clientName string, clientNamespace string, dst string, expectedOutput string, expectErr bool) {
@@ -1325,6 +1364,21 @@ var _ = ginkgo.DescribeTableSubtree("BGP: isolation between advertised networks"
 					nodePortA := svcNodePortETPLocalNetA.Spec.Ports[0].NodePort
 					out := ""
 					errBool := false
+
+					// FIXME https://github.com/ovn-kubernetes/ovn-kubernetes/issues/5531
+					// For LGW mode, nodePort service with ETP=local is not working for UDN networks
+					if IsGatewayModeLocal(f.ClientSet) {
+						out = curlConnectionTimeoutCode
+						errBool = true
+						if cudnATemplate.Spec.Network.Topology == udnv1.NetworkTopologyLayer3 && ipFamily == utilnet.IPv4 {
+							out = "56"
+							errBool = true
+						}
+						if cudnATemplate.Spec.Network.Topology == udnv1.NetworkTopologyLayer2 && ipFamily == utilnet.IPv4 {
+							out = ""
+							errBool = false
+						}
+					}
 					return clientPod.Name, clientPod.Namespace, net.JoinHostPort(nodeIP, fmt.Sprint(nodePortA)), out, errBool
 				}),
 		)
