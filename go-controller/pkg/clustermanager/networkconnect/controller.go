58510a84fce616bc403cd26057efdd90d2c365b8
Surya Seetharaman <suryaseetharaman.9@gmail.com>
2025-10-05 23:43:50 +0200

    CM: Add network-connect controller plumbing
    Signed-off-by: Surya Seetharaman <suryaseetharaman.9@gmail.com>

---
+++ go-controller/pkg/clustermanager/networkconnect/controller.go
@@ -0,0 +1,189 @@
+package networkconnect
+
+import (
+	"reflect"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	corelisters "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog/v2"
+
+	nadv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
+	nadclientset "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/client/clientset/versioned"
+	nadlisters "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/client/listers/k8s.cni.cncf.io/v1"
+	controllerutil "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/controller"
+	networkconnectv1 "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/clusternetworkconnect/v1"
+	networkconnectclientset "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/clusternetworkconnect/v1/apis/clientset/versioned"
+	networkconnectlisters "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/clusternetworkconnect/v1/apis/listers/clusternetworkconnect/v1"
+	userdefinednetworkv1 "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/userdefinednetwork/v1"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/factory"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/networkmanager"
+	ovntypes "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
+)
+
+var (
+	cudnController = userdefinednetworkv1.SchemeGroupVersion.WithKind("ClusterUserDefinedNetwork")
+	udnController  = userdefinednetworkv1.SchemeGroupVersion.WithKind("UserDefinedNetwork")
+)
+
+type Controller struct {
+	// wf is the watch factory for accessing informers
+	wf *factory.WatchFactory
+	// listers
+	cncLister       networkconnectlisters.ClusterNetworkConnectLister
+	namespaceLister corelisters.NamespaceLister
+	nadLister       nadlisters.NetworkAttachmentDefinitionLister
+	//clientset
+	cncClient networkconnectclientset.Interface
+	nadClient nadclientset.Interface
+	// Controller for managing cluster-network-connect events
+	cncController controllerutil.Controller
+	// Controller for managing NetworkAttachmentDefinition events
+	nadController  controllerutil.Controller
+	networkManager networkmanager.Interface
+}
+
+func NewController(
+	wf *factory.WatchFactory,
+	ovnClient *util.OVNClusterManagerClientset,
+	networkManager networkmanager.Interface,
+) *Controller {
+	cncLister := wf.ClusterNetworkConnectInformer().Lister()
+	nadLister := wf.NADInformer().Lister()
+
+	c := &Controller{
+		wf:              wf,
+		cncClient:       ovnClient.NetworkConnectClient,
+		nadClient:       ovnClient.NetworkAttchDefClient,
+		cncLister:       cncLister,
+		nadLister:       nadLister,
+		namespaceLister: wf.NamespaceInformer().Lister(),
+		networkManager:  networkManager,
+	}
+
+	cncCfg := &controllerutil.ControllerConfig[networkconnectv1.ClusterNetworkConnect]{
+		RateLimiter:    workqueue.DefaultTypedControllerRateLimiter[string](),
+		Informer:       wf.ClusterNetworkConnectInformer().Informer(),
+		Lister:         cncLister.List,
+		Reconcile:      c.reconcileClusterNetworkConnect,
+		ObjNeedsUpdate: cncNeedsUpdate,
+		Threadiness:    1,
+	}
+	c.cncController = controllerutil.NewController(
+		"clustermanager-network-connect-controller",
+		cncCfg,
+	)
+
+	nadCfg := &controllerutil.ControllerConfig[nadv1.NetworkAttachmentDefinition]{
+		RateLimiter:    workqueue.DefaultTypedControllerRateLimiter[string](),
+		Informer:       wf.NADInformer().Informer(),
+		Lister:         nadLister.List,
+		Reconcile:      c.reconcileNAD,
+		ObjNeedsUpdate: nadNeedsUpdate,
+		Threadiness:    1,
+	}
+	c.nadController = controllerutil.NewController(
+		"clustermanager-network-connect-network-attachment-definition-controller",
+		nadCfg,
+	)
+
+	return c
+}
+
+func (c *Controller) Start() error {
+	defer klog.Infof("Cluster manager network connect controllers started")
+	return controllerutil.Start(
+		c.cncController,
+		c.nadController,
+	)
+}
+
+func (c *Controller) Stop() {
+	controllerutil.Stop(
+		c.cncController,
+		c.nadController,
+	)
+	klog.Infof("Cluster manager network connect controllers stopped")
+}
+
+func cncNeedsUpdate(oldObj, newObj *networkconnectv1.ClusterNetworkConnect) bool {
+	// Case 1: CNC is being deleted
+	// Case 2: CNC is being created
+	if oldObj == nil || newObj == nil {
+		return true
+	}
+	// Case 3: CNC is being updated
+	// Only trigger updates when the Spec.NetworkSelectors changes
+	// We only need to check for selector changes
+	// and don't need to react on connectivity enabled field changes
+	// from cluster manager.
+	// connectSubnet is immutable so that can't change after creation.
+	return !reflect.DeepEqual(oldObj.Spec.NetworkSelectors, newObj.Spec.NetworkSelectors)
+}
+
+func nadNeedsUpdate(oldObj, newObj *nadv1.NetworkAttachmentDefinition) bool {
+	nadSupported := func(nad *nadv1.NetworkAttachmentDefinition) bool {
+		if nad == nil {
+			return false
+		}
+		// we don't support direct NADs anymore. CNC is only supported for CUDNs and UDNs
+		controller := metav1.GetControllerOfNoCopy(nad)
+		isCUDN := controller != nil && controller.Kind == cudnController.Kind && controller.APIVersion == cudnController.GroupVersion().String()
+		isUDN := controller != nil && controller.Kind == udnController.Kind && controller.APIVersion == udnController.GroupVersion().String()
+		if !isCUDN && !isUDN {
+			return false
+		}
+		network, err := util.ParseNADInfo(newObj)
+		if err != nil {
+			// cannot parse NAD info, so we take this as unsupported NAD error
+			return false
+		}
+		if network.IsPrimaryNetwork() {
+			// only layer3 and layer2 topology are supported
+			// but since primary network is always layer3 or layer2,
+			// we can ignored the need to check the topology
+			return true
+		}
+		return false // we don't support secondary networks, so we can ignore it
+	}
+	// ignore if we don't support this NAD
+	if !nadSupported(oldObj) && !nadSupported(newObj) {
+		return false
+	}
+	oldNADLabels := labels.Set{}
+	newNADLabels := labels.Set{}
+	if oldObj != nil {
+		oldNADLabels = labels.Set(oldObj.Labels)
+	}
+	if newObj != nil {
+		newNADLabels = labels.Set(newObj.Labels)
+	}
+	labelsChanged := !labels.Equals(oldNADLabels, newNADLabels)
+	// safe spot check for ovn network id annotation add happening as update to NADs
+	var annotationsChanged bool
+	if oldObj != nil && newObj != nil {
+		annotationsChanged = oldObj.Annotations[ovntypes.OvnNetworkIDAnnotation] != newObj.Annotations[ovntypes.OvnNetworkIDAnnotation]
+	}
+	// CASE1: NAD is being deleted (UDN or CUDN is being deleted)
+	// CASE2: NAD is being created (UDN or CUDN is being created)
+	// CASE3: NAD is being updated (UDN or CUDN is being updated)
+	//  3.1: NAD network id annotation changed
+	//  3.2: NAD labels changed (only relevant for CUDNs->NADs)
+	return oldObj == nil || newObj == nil || labelsChanged || annotationsChanged
+}
+
+func (c *Controller) reconcileClusterNetworkConnect(key string) error {
+	// STEP1: Validate the CNC
+	// STEP2: Discover the selected UDNs and CUDNs
+	// STEP3: Generate subnets of size CNC.Spec.ConnectSubnets.NetworkPrefix for each layer3 network
+	//  and /31 subnet for each layer2 networks
+	// STEP4: Generate a tunnelID for the connect router corresponding to this CNC
+
+	return nil
+}
+
+func (c *Controller) reconcileNAD(key string) error {
+	return nil
+}
