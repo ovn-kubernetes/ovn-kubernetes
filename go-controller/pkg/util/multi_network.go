85c05252e256e510a83df0b9e51180fd491cdf4f
Peng Liu <pliu@redhat.com>
2025-10-24 08:27:58 +0000

    no-overlay: Add 'transport' field to OVN-K CNI NetConf
    Add transport field to NetConf to indicate the transport technology
    used by a network (e.g., "noOverlay" or "geneve").
    
    This field is set in the NAD's spec.config JSON and used by NetInfo
    to determine the network's transport mode. The field should be managed
    by OVN-Kubernetes and not set manually by users.
    
    Changes:
    - Add Transport field to NetConf struct
    - Add GetNetworkTransport() method to NetInfo interface
    - Add NetworkTransportGeneve and NetworkTransportNoOverlay constants
    - Update all NetInfo implementations to support transport field
    
    Based on OKEP-5259: No-overlay Mode For Layer-3 Networks using BGP
    
    Signed-off-by: Peng Liu <pliu@redhat.com>

--- go-controller/pkg/util/multi_network.go
+++ go-controller/pkg/util/multi_network.go
@@ -79,6 +79,8 @@ type NetInfo interface {
 	// GetEgressIPAdvertisedNodes return the nodes where egress IP are
 	// advertised.
 	GetEgressIPAdvertisedNodes() []string
+	// GetNetworkTransport returns the transport technology used by this network.
+	GetNetworkTransport() string
 
 	// derived information.
 	GetNADNamespaces() []string
@@ -396,6 +398,10 @@ func (nInfo *mutableNetInfo) GetEgressIPAdvertisedNodes() []string {
 	return maps.Keys(nInfo.eipAdvertisements)
 }
 
+func (nInfo *mutableNetInfo) GetNetworkTransport() string {
+	return ""
+}
+
 // GetNADs returns all the NADs associated with this network
 func (nInfo *mutableNetInfo) GetNADs() []string {
 	nInfo.RLock()
@@ -693,6 +699,10 @@ func (nInfo *DefaultNetInfo) GetNodeManagementIP(hostSubnet *net.IPNet) *net.IPN
 	return GetNodeManagementIfAddr(hostSubnet)
 }
 
+func (nInfo *DefaultNetInfo) GetNetworkTransport() string {
+	return config.Default.Transport
+}
+
 // userDefinedNetInfo holds the network name information for a User Defined Network if non-nil
 type userDefinedNetInfo struct {
 	mutableNetInfo
@@ -705,6 +715,7 @@ type userDefinedNetInfo struct {
 	mtu                int
 	vlan               uint
 	allowPersistentIPs bool
+	transport          string
 
 	ipv4mode, ipv6mode    bool
 	subnets               []config.CIDRNetworkEntry
@@ -915,6 +926,10 @@ func (nInfo *userDefinedNetInfo) TransitSubnets() []*net.IPNet {
 	return nInfo.transitSubnets
 }
 
+func (nInfo *userDefinedNetInfo) GetNetworkTransport() string {
+	return nInfo.transport
+}
+
 func (nInfo *userDefinedNetInfo) canReconcile(other NetInfo) bool {
 	if (nInfo == nil) != (other == nil) {
 		return false
@@ -949,6 +964,9 @@ func (nInfo *userDefinedNetInfo) canReconcile(other NetInfo) bool {
 		return false
 	}
 
+	if nInfo.transport != other.GetNetworkTransport() {
+		return false
+	}
 	lessCIDRNetworkEntry := func(a, b config.CIDRNetworkEntry) bool { return a.String() < b.String() }
 	if !cmp.Equal(nInfo.subnets, other.Subnets(), cmpopts.SortSlices(lessCIDRNetworkEntry)) {
 		return false
