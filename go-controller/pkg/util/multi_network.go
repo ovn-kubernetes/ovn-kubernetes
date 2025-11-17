d0f4d284b32a8caab492fccba1c2ec6838612b90
Peng Liu <pliu@redhat.com>
2025-11-17 15:38:48 +0800

    no-overlay: Add GetOutboundSNAT() to NetInfo
    Add GetOutboundSNAT() method to the NetInfo interface and
    its implementations to query whether outbound SNAT is enabled
    for a network.
    
    This is part of the no-overlay mode implementation (OKEP-5259),
    which supports configurable SNAT behavior for pod egress traffic:
    
    - When enabled: Pod egress traffic to external destinations is
      SNATed to the node IP, while pod-to-pod traffic is exempted
      from SNAT using the node subnets address set.
    
    - When disabled: Pod egress traffic is not SNATed, requiring
      pod IPs to be routable on the external network.
    
    Implementation details:
    - DefaultNetInfo checks config.NoOverlay.OutboundSNAT == "enable"
    - userDefinedNetInfo currently returns false (UDN support to be
      added in a future commit)
    - mutableNetInfo returns false as a default implementation
    
    The method is used by gateway and SNAT logic to determine the
    appropriate match conditions for SNAT rules.
    
    Signed-off-by: Peng Liu <pliu@redhat.com>

--- go-controller/pkg/util/multi_network.go
+++ go-controller/pkg/util/multi_network.go
@@ -58,6 +58,9 @@ type NetInfo interface {
 	PhysicalNetworkName() string
 	GetNodeGatewayIP(hostSubnet *net.IPNet) *net.IPNet
 	GetNodeManagementIP(hostSubnet *net.IPNet) *net.IPNet
+	// GetNetworkTransport returns the transport technology used by this network.
+	GetNetworkTransport() string
+	GetOutboundSNAT() string
 
 	// dynamic information, can change over time
 	GetNADs() []string
@@ -79,8 +82,6 @@ type NetInfo interface {
 	// GetEgressIPAdvertisedNodes return the nodes where egress IP are
 	// advertised.
 	GetEgressIPAdvertisedNodes() []string
-	// GetNetworkTransport returns the transport technology used by this network.
-	GetNetworkTransport() string
 
 	// derived information.
 	GetNADNamespaces() []string
@@ -402,6 +403,10 @@ func (nInfo *mutableNetInfo) GetNetworkTransport() string {
 	return ""
 }
 
+func (nInfo *mutableNetInfo) GetOutboundSNAT() string {
+	return ""
+}
+
 // GetNADs returns all the NADs associated with this network
 func (nInfo *mutableNetInfo) GetNADs() []string {
 	nInfo.RLock()
@@ -703,6 +708,10 @@ func (nInfo *DefaultNetInfo) GetNetworkTransport() string {
 	return config.Default.Transport
 }
 
+func (nInfo *DefaultNetInfo) GetOutboundSNAT() string {
+	return config.NoOverlay.OutboundSNAT
+}
+
 // userDefinedNetInfo holds the network name information for a User Defined Network if non-nil
 type userDefinedNetInfo struct {
 	mutableNetInfo
@@ -928,6 +937,10 @@ func (nInfo *userDefinedNetInfo) TransitSubnets() []*net.IPNet {
 
 func (nInfo *userDefinedNetInfo) GetNetworkTransport() string {
 	return nInfo.transport
+}
+
+func (nInfo *userDefinedNetInfo) GetOutboundSNAT() string {
+	return ""
 }
 
 func (nInfo *userDefinedNetInfo) canReconcile(other NetInfo) bool {
