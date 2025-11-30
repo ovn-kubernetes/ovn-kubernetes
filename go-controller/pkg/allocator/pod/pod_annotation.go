76144d06ec95d48aad97cc1a77d96949e9f282eb
Ram Lavi <ralavi@redhat.com>
2025-11-30 10:43:42 +0200

    allocator, persistentips: update IPAM claim status for both success and failure
    Add updateIPAMClaimStatus helper function to set IPAM claim status
    conditions for both successful and failed IP allocations.
    Previously, IPAM claim status was only updated on successful allocation.
    
    The new helper function:
    - Sets OwnerPod and IPs in status
    - Sets IPsAllocated condition (success/failure conditions)
    - failure conditions are: SubnetExhausted(ErrFull), IPAddressConflict
    (ErrAllocated), and ReserveMACConflict.
    
    the helper is called inside defer to make sure ipam.status.conditions
    are updated even in case of early return. defers in go run in LIFO so
    the rollback defer will run last, as it should.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- go-controller/pkg/allocator/pod/pod_annotation.go
+++ go-controller/pkg/allocator/pod/pod_annotation.go
@@ -400,6 +400,16 @@ func allocatePodAnnotationWithRollback(
 		hasIPAMClaim = ipamClaim != nil && len(ipamClaim.Status.IPs) > 0
 	}
 
+	defer func() {
+		if ipamClaim == nil || claimsReconciler == nil {
+			return
+		}
+		updatedClaim := claimsReconciler.UpdateIPAMClaimStatus(ipamClaim, podAnnotation, pod.Name, err)
+		if reconcileErr := claimsReconciler.Reconcile(ipamClaim, updatedClaim, ipAllocator); reconcileErr != nil {
+			err = errors.Join(err, fmt.Errorf("failed to reconcile IPAM claim %s/%s: %w", ipamClaim.Namespace, ipamClaim.Name, reconcileErr))
+		}
+	}()
+
 	if hasIPAM && hasStaticIPRequest {
 		if err = validateStaticIPRequest(netInfo, network, ipamClaim, podDesc); err != nil {
 			return
@@ -501,12 +511,6 @@ func allocatePodAnnotationWithRollback(
 		updatedPod = pod
 		updatedPod.Annotations, err = util.MarshalPodAnnotation(updatedPod.Annotations, tentative, nadName)
 		podAnnotation = tentative
-	}
-
-	if ipamClaim != nil && err == nil {
-		newIPAMClaim := ipamClaim.DeepCopy()
-		newIPAMClaim.Status.IPs = util.StringSlice(podAnnotation.IPs)
-		err = claimsReconciler.Reconcile(ipamClaim, newIPAMClaim, ipAllocator)
 	}
 
 	return
