600129a39e400576714ccab364b0d253a9caed3a
Lei Huang <leih@nvidia.com>
2025-12-22 16:04:11 -0800

    Add a feature gate for multi-VTEP
    Add the OVN_MULTI_VTEP_ENABLE env var to explicitly enable multi-VTEP
    and prevent multi-VTEP code paths from running on normal setup.
    
    Signed-off-by: Lei Huang <leih@nvidia.com>

--- dist/images/ovnkube.sh
+++ dist/images/ovnkube.sh
@@ -265,6 +265,8 @@ ovn_egressqos_enable=${OVN_EGRESSQOS_ENABLE:-false}
 ovn_egressservice_enable=${OVN_EGRESSSERVICE_ENABLE:-false}
 #OVN_MULTI_NETWORK_ENABLE - enable multiple network support for ovn-kubernetes
 ovn_multi_network_enable=${OVN_MULTI_NETWORK_ENABLE:-false}
+#OVN_MULTI_VTEP_ENABLE - enable multiple VTEP support for ovn-kubernetes
+ovn_multi_vtep_enable=${OVN_MULTI_VTEP_ENABLE:-false}
 #OVN_NETWORK_SEGMENTATION_ENABLE - enable user defined primary networks for ovn-kubernetes
 ovn_network_segmentation_enable=${OVN_NETWORK_SEGMENTATION_ENABLE:=false}
 #OVN_NETWORK_CONNECT_ENABLE - enable network connect for ovn-kubernetes
@@ -1296,6 +1298,12 @@ ovn-master() {
   fi
   echo "multi_network_enabled_flag=${multi_network_enabled_flag}"
 
+  ovn_multi_vtep_enable_flag=
+  if [[ ${ovn_multi_vtep_enable} == "true" ]]; then
+      ovn_multi_vtep_enable_flag="--enable-multi-vtep"
+  fi
+  echo "ovn_multi_vtep_enable_flag=${ovn_multi_vtep_enable_flag}"
+
   network_segmentation_enabled_flag=
   if [[ ${ovn_network_segmentation_enable} == "true" ]]; then
 	  network_segmentation_enabled_flag="--enable-multi-network --enable-network-segmentation"
@@ -1417,6 +1425,7 @@ ovn-master() {
     ${libovsdb_client_logfile_flag} \
     ${multicast_enabled_flag} \
     ${multi_network_enabled_flag} \
+    ${ovn_multi_vtep_enable_flag} \
     ${network_segmentation_enabled_flag} \
     ${route_advertisements_enabled_flag} \
     ${advertised_udn_isolation_flag} \
@@ -1604,6 +1613,12 @@ ovnkube-controller() {
   fi
   echo "multi_network_enabled_flag=${multi_network_enabled_flag}"
 
+  ovn_multi_vtep_enable_flag=
+  if [[ ${ovn_multi_vtep_enable} == "true" ]]; then
+      ovn_multi_vtep_enable_flag="--enable-multi-vtep"
+  fi
+  echo "ovn_multi_vtep_enable_flag=${ovn_multi_vtep_enable_flag}"
+
   network_segmentation_enabled_flag=
   if [[ ${ovn_network_segmentation_enable} == "true" ]]; then
 	  network_segmentation_enabled_flag="--enable-multi-network --enable-network-segmentation"
@@ -1747,6 +1762,7 @@ ovnkube-controller() {
     ${libovsdb_client_logfile_flag} \
     ${multicast_enabled_flag} \
     ${multi_network_enabled_flag} \
+    ${ovn_multi_vtep_enable_flag} \
     ${network_segmentation_enabled_flag} \
     ${network_connect_enabled_flag} \
     ${pre_conf_udn_addr_enable_flag} \
@@ -1948,6 +1964,12 @@ ovnkube-controller-with-node() {
 	  multi_network_enabled_flag="--enable-multi-network --enable-multi-networkpolicy"
   fi
   echo "multi_network_enabled_flag=${multi_network_enabled_flag}"
+
+  ovn_multi_vtep_enable_flag=
+  if [[ ${ovn_multi_vtep_enable} == "true" ]]; then
+      ovn_multi_vtep_enable_flag="--enable-multi-vtep"
+  fi
+  echo "ovn_multi_vtep_enable_flag=${ovn_multi_vtep_enable_flag}"
 
   network_segmentation_enabled_flag=
   if [[ ${ovn_network_segmentation_enable} == "true" ]]; then
@@ -2232,6 +2254,7 @@ ovnkube-controller-with-node() {
     ${monitor_all} \
     ${multicast_enabled_flag} \
     ${multi_network_enabled_flag} \
+    ${ovn_multi_vtep_enable_flag} \
     ${network_segmentation_enabled_flag} \
     ${network_connect_enabled_flag} \
     ${pre_conf_udn_addr_enable_flag} \
@@ -2397,6 +2420,12 @@ ovn-cluster-manager() {
   fi
   echo "multi_network_enabled_flag: ${multi_network_enabled_flag}"
 
+  ovn_multi_vtep_enable_flag=
+  if [[ ${ovn_multi_vtep_enable} == "true" ]]; then
+      ovn_multi_vtep_enable_flag="--enable-multi-vtep"
+  fi
+  echo "ovn_multi_vtep_enable_flag=${ovn_multi_vtep_enable_flag}"
+
   network_segmentation_enabled_flag=
   if [[ ${ovn_network_segmentation_enable} == "true" ]]; then
 	  network_segmentation_enabled_flag="--enable-multi-network --enable-network-segmentation"
@@ -2486,6 +2515,7 @@ ovn-cluster-manager() {
     ${hybrid_overlay_flags} \
     ${multicast_enabled_flag} \
     ${multi_network_enabled_flag} \
+    ${ovn_multi_vtep_enable_flag} \
     ${network_segmentation_enabled_flag} \
     ${network_connect_enabled_flag} \
     ${pre_conf_udn_addr_enable_flag} \
@@ -2656,6 +2686,12 @@ ovn-node() {
   if [[ ${ovn_multi_network_enable} == "true" ]]; then
 	  multi_network_enabled_flag="--enable-multi-network --enable-multi-networkpolicy"
   fi
+
+  ovn_multi_vtep_enable_flag=
+  if [[ ${ovn_multi_vtep_enable} == "true" ]]; then
+      ovn_multi_vtep_enable_flag="--enable-multi-vtep"
+  fi
+  echo "ovn_multi_vtep_enable_flag=${ovn_multi_vtep_enable_flag}"
 
   network_segmentation_enabled_flag=
   if [[ ${ovn_network_segmentation_enable} == "true" ]]; then
@@ -2911,6 +2947,7 @@ ovn-node() {
         ${monitor_all} \
         ${multicast_enabled_flag} \
         ${multi_network_enabled_flag} \
+        ${ovn_multi_vtep_enable_flag} \
         ${network_segmentation_enabled_flag} \
         ${network_connect_enabled_flag} \
         ${pre_conf_udn_addr_enable_flag} \
