374f259d24c3716a5b8f0fd995e9a507708b135b
Jamo Luhrsen <jluhrsen@gmail.com>
2026-08-17 14:03:21 -0700

    e2e: retry NodePort probe in hairpin SNAT test
    pokeEndpointViaNode returns "" until the NodePort is programmed on the
    target node, so a single probe can fail. Wrap it in a PollImmediate loop
    to tolerate asynchronous dataplane programming.
    
    Signed-off-by: Jamo Luhrsen <jluhrsen@gmail.com>

--- test/e2e/service.go
+++ test/e2e/service.go
@@ -2614,7 +2614,16 @@ var _ = ginkgo.Describe("Service Hairpin SNAT", feature.Service, func() {
 
 		ginkgo.By("by sending a TCP packet to service service-for-pods with type=NodePort(" + nodeIP + ":" + fmt.Sprint(svc.Spec.Ports[0].NodePort) + ") in namespace " + namespaceName + " from node " + backendNodeName)
 
-		clientIP := pokeEndpointViaNode(backendNodeName, "http", nodeIP, hostNetPort, uint16(svc.Spec.Ports[0].NodePort), "clientip")
+		var clientIP string
+		err = wait.PollImmediate(30*time.Second, 3*time.Minute, func() (bool, error) {
+			clientIP = pokeEndpointViaNode(backendNodeName, "http", nodeIP, hostNetPort, uint16(svc.Spec.Ports[0].NodePort), "clientip")
+			if clientIP == "" {
+				return false, nil
+			}
+			return true, nil
+		})
+		framework.ExpectNoError(err, "timed out waiting for successful NodePort response")
+
 		clientIP, _, err = net.SplitHostPort(clientIP)
 		framework.ExpectNoError(err, "failed to parse client ip:port")
 
