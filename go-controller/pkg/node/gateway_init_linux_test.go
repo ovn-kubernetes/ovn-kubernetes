5d65833f12c4c17ae7767f460cbbba8d9503e27b
Konstantinos Karampogias <karampok@gmail.com>
2026-09-04 13:47:14 +0200

    egressservice: restore ClusterIP connectivity
    Refactor the node controller to install the required fwmark
    policy-routing rules during startup. These rules route packets marked
    for node SNAT through the main routing table, preserving ClusterIP
    connectivity.
    
    The rules are shared by EgressService and EgressIP.
    Start ruleManager when egressX feature enabled for shared gateway
    
    Signed-off-by: Konstantinos Karampogias <karampok@gmail.com>

--- go-controller/pkg/node/gateway_init_linux_test.go
+++ go-controller/pkg/node/gateway_init_linux_test.go
@@ -910,7 +910,7 @@ func shareGatewayInterfaceDPUHostTest(app *cli.App, testNS ns.NetNS, uplinkName,
 		ipnet.IP = ip
 		routeManager := routemanager.NewController()
 		cnnci := NewCommonNodeNetworkControllerInfo(kubeFakeClient, fakeClient.AdminPolicyRouteClient, wf, nil, nodeName, routeManager)
-		nc := newDefaultNodeNetworkController(cnnci, stop, wg, routeManager, nil, nil)
+		nc := newDefaultNodeNetworkController(cnnci, stop, wg, routeManager, nil, nil, nil)
 		// must run route manager manually which is usually started with nc.Start()
 		wg.Add(1)
 		go func() {
