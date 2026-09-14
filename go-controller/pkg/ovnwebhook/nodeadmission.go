93d59ac5b855f75fd0af33314302d23e836eaf2d
Yossi Boaron <yboaron@redhat.com>
2026-09-14 18:18:25 +0300

    gateway: fix SNAT masquerading for excluded subnets in local gateway mode
    In local gateway mode, pod egress traffic goes through
    ovn-kube-local-gw-masq -> ovn-kube-pod-subnet-masq nftables chains
    which unconditionally masquerade the pod source IP to the node IP.
    Add ip daddr @mgmtport-no-snat-subnets-v4/v6 return as the first
    rules in ovn-kube-pod-subnet-masq so excluded-subnet traffic returns
    before being masqueraded.
    
    Introduce k8s.ovn.org/node-snat-exclude-subnets as a neutral
    replacement for the deprecated node-ingress-snat-exclude-subnets
    annotation. The new annotation controls SNAT exclusion for both
    ingress and egress traffic directions. The deprecated annotation is
    still honored for backward compatibility. Both annotations are read
    and their subnet lists merged with deduplication.
    
    Extract getNoSNATSubnetsReturnRules into a shared helper to eliminate
    duplication between getLocalGatewayNATNFTRules and
    addOrUpdateLocalGatewayPodSubnetNFTRules.
    
    This PR is a rebased version of #6030 by @pacevedom, updated to
    resolve merge conflicts with the current master.
    
    Fixes #6799
    
    Signed-off-by: Yossi Boaron <yboaron@redhat.com>

--- go-controller/pkg/ovnwebhook/nodeadmission.go
+++ go-controller/pkg/ovnwebhook/nodeadmission.go
@@ -37,6 +37,7 @@ var commonNodeAnnotationChecks = map[string]checkNodeAnnot{
 	util.OvnNodeGatewayMtuSupport:          nil,
 	util.OvnNodeManagementPort:             nil,
 	util.OvnNodeDontSNATSubnets:            nil,
+	util.OvnNodeSNATExcludeSubnets:         nil,
 	util.OVNNodePrimaryDPUHostAddr:         nil,
 	util.OvnNodeChassisID: func(v annotationChange, _ string) error {
 		if v.action == removed {
